1df4d3e5713cd5bfc7ea558e5f05f971d73d75fc
Yao Jean-Claude <jeandev84@yahoo.com>
2019-11-30 18:08:19 +0300

    Add BreadCrumb

--- services/BreadCrumb/BreadCrumb.php
+++ services/BreadCrumb/BreadCrumb.php
@@ -9,4 +9,55 @@ namespace BreadCrumb;
 class BreadCrumb
 {
 
+    /** @var string */
+    private $breadcrumb;
+
+    /** @var string  */
+    private $separator = ' / ';
+
+    /** @var string  */
+    private $domain = 'https://ddmseo.com';
+
+
+    /**
+     * @param $array
+     * @return string
+    */
+    public function build($array)
+    {
+        $breadcrumbs = array_merge(array('home' => ''), $array);
+
+        $count = 0;
+
+        foreach($breadcrumbs as $title => $link) {
+            $this->breadcrumb .= '
+         <span itemscope="" itemtype="http://data-vocabulary.org/Breadcrumb">
+            <a href="'.$this->domain. '/'.$link.'" itemprop="url">
+               <span itemprop="title">'.$title.'</span>
+            </a>
+         </span>';
+
+            $count++;
+
+            if($count !== count($breadcrumbs)) {
+                $this->breadcrumb .= $this->separator;
+            }
+        }
+        return $this->breadcrumb;
+    }
 }
+
+/**
+ * Testing
+*/
+
+$breadcrumb = new breadcrumb();
+
+echo $breadcrumb->build(array(
+    'Development' => 'development',
+    'PHP Breadcrumb' => 'php-breadcrumb.html'
+));
+
+/*
+Home / Development / PHP Breadcrumb // the HTML output with links
+*/
